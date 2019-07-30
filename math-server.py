e7bbd8e58b21309bab7ccbab7c3f0c5aad6de390
Wenchang Liu <wenchang.liu@student.manchester.ac.uk>
2019-07-30 16:48:51 +0800

    change server settings back to specific ip

--- math-server.py
+++ math-server.py
@@ -10,5 +10,5 @@ def home():
     return render_template('echart-demo.html', title="Home")
 
 if __name__ == '__main__':
-    # app.run(debug=True, host='10.110.165.244', port=5000)
-    app.run(debug=True)
+    app.run(debug=True, host='10.110.165.244', port=5000)
+    # app.run(debug=True)
