74e82c458eff4bfbcd6648b1a7a7c82d35fa650b
Wenchang Liu <wenchang.liu@student.manchester.ac.uk>
2019-08-01 15:52:16 +0800

    add GUI to edit the graph
    including add/delete nodes/edges, but need ctrl+f5 to reload resources

--- test-networkx.py
+++ test-networkx.py
@@ -4,17 +4,16 @@ from networkx.readwrite import json_graph
 import json
 
 # G = gexf.read_gexf("static/data/middle_school_extend.gexf")
-# print(nx.info(G))
-# G.add_node('test', modular=1, Degree=0, label='test')
-# G.remove_node('test')
-# print(G.nodes['Circle'])
-# print(nx.info(G))
-# G = gexf.write_gexf(G, "static/data/middle_school_extend.gexf")
-# test_data = json_graph.node_link_data(G)
-# with open("static/data/middle_school_extend.json", "w") as write_file:
-#     json.dump(test_data, write_file)
-
 with open("static/data/middle_school_extend.json", "r") as read_file:
-    test_data = json.load(read_file)
-G = json_graph.node_link_graph(test_data)
+    data = json.load(read_file)
+G = json_graph.node_link_graph(data)
+print(nx.info(G))
+G.add_node('test1', modular=1, Degree=0, viz={'size': 50}, label='test1')
+# G.remove_node('test')
 print(G.nodes['Circle'])
+print(G.nodes['test1'])
+print(nx.info(G))
+# G = gexf.write_gexf(G, "static/data/middle_school_extend.gexf")
+data = json_graph.node_link_data(G)
+with open("static/data/middle_school_extend.json", "w") as write_file:
+    json.dump(data, write_file)
