b714d7df3d17365ff4d72a5d1bef434202d5fdb4
Wenchang Liu <wenchang.liu@student.manchester.ac.uk>
2019-08-02 16:35:40 +0800

    automatically set degree/size when add/delete edge

--- math-server.py
+++ math-server.py
@@ -31,6 +31,26 @@ class edge_form(Form):
     delete_edge = SubmitField("Delete Edge")
 
 
+def update_attr(G, form2):
+    # calculate new degree and size, max_size = 30, min_size = 10
+    list_degree = [d for n, d in G.degree()]
+    degree_max = max(list_degree)
+    degree_min = min(list_degree)
+    degree_source = G.degree(nbunch=form2.source_name.data)
+    degree_target = G.degree(nbunch=form2.target_name.data)
+    if degree_source >= degree_max:
+        size_source = 30
+    else:
+        size_source = (degree_source - degree_min) * (30-10) / (degree_max-degree_min) + 10
+    if degree_target >= degree_max:
+        size_target = 30
+    else:
+        size_target = (degree_target - degree_min) * (30-10) / (degree_max-degree_min) + 10
+    attrs = {form2.source_name.data: {'Degree': degree_source, 'viz': {'size': size_source}}, 
+            form2.target_name.data: {'Degree': degree_target, 'viz': {'size': size_target}}}
+    nx.set_node_attributes(G, attrs)
+
+
 @app.route('/')
 @app.route('/editGraph', methods=['get', 'post'])
 
@@ -49,12 +69,14 @@ def edit_graph():
         if form1.add_node.data and form1.validate():
 
             if G.has_node(form1.node_name.data):
-                print("add failed: this node already exist")
+                print("add failed: such node already exist")
             else:
                 G.add_node(form1.node_name.data, 
                             modular=form1.category.data, 
-                            Degree=0, viz={'size': 50}, 
-                            label=form1.node_name.data)
+                            Degree=0, viz={'size': 10}, 
+                            label=form1.node_name.data,
+                            content=form1.content.data,
+                            notes=form1.notes.data)
                 print("added a node")
         # else:
         #     print(form1.validate())
@@ -66,24 +88,30 @@ def edit_graph():
                 G.remove_node(form1.node_name.data)
                 print("removed a node")
             else:
-                print("remove failed: this node does not exist")
+                print("remove failed: such node does not exist")
         # else:
         #     print(form1.validate())
 
         # add edge
         if form2.add_edge.data and form2.validate():
-
-            G.add_edge(form2.source_name.data, form2.target_name.data, relationship=form2.relationship.data)
-            print("added an edge")
+            
+            if G.has_edge(form2.source_name.data, form2.target_name.data, key=form2.relationship.data):
+                print("add failed: such edge already exist")
+            else:
+                G.add_edge(form2.source_name.data, form2.target_name.data, relationship=form2.relationship.data, key=form2.relationship.data)
+                update_attr(G, form2)
+                print("added an edge")
         
         # delete edge
         if form2.delete_edge.data and form2.validate():
             
             if G.has_edge(form2.source_name.data, form2.target_name.data):
                 G.remove_edge(form2.source_name.data, form2.target_name.data)
+                update_attr(G, form2)
                 print("deleted an edge")
+
             else:
-                print("remove failed: this edge does not exist")
+                print("remove failed: such edge does not exist")
 
         # save edited graph G into json file
         data = json_graph.node_link_data(G)
--- test-networkx.py
+++ test-networkx.py
@@ -8,12 +8,16 @@ with open("static/data/middle_school_extend.json", "r") as read_file:
     data = json.load(read_file)
 G = json_graph.node_link_graph(data)
 print(nx.info(G))
-G.add_node('test1', modular=1, Degree=0, viz={'size': 50}, label='test1')
+# G.add_node('test1', modular=1, Degree=0, viz={'size': 50}, label='test1')
 # G.remove_node('test')
-print(G.nodes['Circle'])
-print(G.nodes['test1'])
+# print(G.nodes['Circle'])
+# print(G.nodes['test1'])
+degree_max = max([d for n, d in G.degree()])
+print(degree_max)
+
+print("circle: ", nx.degree(G, nbunch='Circle'))
 print(nx.info(G))
 # G = gexf.write_gexf(G, "static/data/middle_school_extend.gexf")
-data = json_graph.node_link_data(G)
-with open("static/data/middle_school_extend.json", "w") as write_file:
-    json.dump(data, write_file)
+# data = json_graph.node_link_data(G)
+# with open("static/data/middle_school_extend.json", "w") as write_file:
+#     json.dump(data, write_file)
