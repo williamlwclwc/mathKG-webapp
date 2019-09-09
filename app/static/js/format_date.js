36c2f5dee294c3caa09831188872efdceed5aaa6
H0radricCube <hutianyi@whu.edu.cn>
2019-09-09 20:46:42 +0800

    timeline shows date when sliding

---
+++ app/static/js/format_date.js
@@ -0,0 +1,51 @@
+
+// (1) yyyy-MM-dd
+// (2) yyyy/MM/dd
+// (3) yyyy-MM-dd hh:mm:ss
+// (4) yyyy/MM/dd hh:mm:ss
+// (5) hh:mm:ss
+
+// 传入毫秒数和时间格式，输出格式化时间
+ms_per_day = 86400000;
+
+function format_Date (date, dateStr, move_d) {
+    if(typeof date === "number"){
+        var d=new Date();
+        d.setTime(date);
+        date=d;
+    }
+    if(move_d !== undefined){
+        // date += ms_per_day * move_d;
+        var date = new Date(Date.parse(date.toString()) + 86400000 * move_d);
+    }
+
+
+    var arr=dateStr.split(/\/|-|:| /);  //分割字符串,- / : 空格
+    var timeArr=[];
+    for (var i = 0; i < arr.length; i++) { //按照需要将日期放入数组timeArr
+        switch (arr[i]) {
+            case "yyyy":
+                timeArr.push(date.getFullYear());           
+                break;
+            case "MM":
+                timeArr.push(date.getMonth()+1);            
+                break;
+            case "dd":
+                timeArr.push(date.getDate());           
+                break;
+            case "hh":
+                timeArr.push(date.getHours());          
+                break;
+            case "mm":
+                timeArr.push(date.getMinutes());        
+                break;
+            case "ss":
+                timeArr.push(date.getSeconds());
+                break;
+        }
+    }
+    for (var i = 0; i <arr.length; i++) {
+        dateStr = dateStr.replace(arr[i],timeArr[i]);
+    }
+    return dateStr;
+}
